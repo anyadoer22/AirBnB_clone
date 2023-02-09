db2974bfe13211b1614116185ed15180465da098
Simon Tagbor <simontagbor360@gmail.com>
2023-02-09 08:32:12 -0800

    add keyword args and non keyworded arg functionality to the init method

--- models/base_model.py
+++ models/base_model.py
@@ -11,10 +11,24 @@ class BaseModel(object):
     """The base class of the Airbnb project
     this class sets attributes that are common to all future subclasses
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        """Ininitalise a new BaseModel
+
+        Args:
+            args (any): unused
+            kwargs (dict): key/value pair of attributes to set
+        """
+        ptrn = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+
+        if len(kwargs) != 0:
+            for k, v in  kwargs.items():
+                if  k == "created_at" or k == "updated":
+                    self.__dict__[k] = datetime.strptime(v, ptrn)
+                else:
+                    self.__dict__[k] = v
 
     def __str__(self):
         """overide default string method"""
