3927e4edd591123debcd58426751ec7aa38335a1
Simon Tagbor <simontagbor360@gmail.com>
2023-02-11 10:46:54 -0800

    fix naming bug

--- models/base_model.py
+++ models/base_model.py
@@ -25,7 +25,7 @@ class BaseModel(object):
 
         if len(kwargs) != 0:
             for k, v in  kwargs.items():
-                if  k == "created_at" or k == "updated":
+                if  k == "created_at" or k == "updated_at":
                     self.__dict__[k] = datetime.strptime(v, ptrn)
                 else:
                     self.__dict__[k] = v
@@ -49,7 +49,7 @@ class BaseModel(object):
         """returns a dictionary containing all keys/values
         of __dict__ of the instance"""
         specialdir = self.__dict__.copy()
-        specialdir["__class__"] = self.__class__.__name__
+        specialdir["__class__"] = "{}".format(self.__class__.__name__)
         specialdir["updated_at"] = self.updated_at.isoformat()
         specialdir["created_at"] = self.created_at.isoformat()
         return specialdir
