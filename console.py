48fc516955ef851c5b603a339b950e8f9cf1376b
Simon Tagbor <simontagbor360@gmail.com>
2023-02-12 16:10:25 -0800

    fix mising methods during merge conflict resolutions

--- console.py
+++ console.py
@@ -56,7 +56,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, line):
         """Create a new instance of BaseModel and print the id"""
-        args = line.split()
+        args = parse(line)
 
         if len(args) == 2:
             objname = args[0]
@@ -69,11 +69,23 @@ class HBNBCommand(cmd.Cmd):
                 storage.save()
         else:
             print("** class name missing **")
+
     def do_show(self, line):
         """prints the String representation of an instance
         usage: show [classname]  [id]
         """
-        args = line.split()
+        args = parse(line)
+        obdict = storage.all()
+        if len(args) == 0:
+            print("** class doesn't exist")
+        elif args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(args[0], args[1]) not in obdict:
+            print("** no instance found **")
+        else:
+            print(obdict["{}.{}".format(args[0], args[1])])
 
     def do_count(self, arg):
         """Usage: count <class> or <class>.count()
@@ -101,10 +113,70 @@ class HBNBCommand(cmd.Cmd):
                     list_obj.append(obj.__str__())
             print(list_obj)
 
-<<<<<<< HEAD
-=======
+    def do_destroy(self, line):
+        """ Usage: destroy <class> <id> or <class>.destroy(<id>)
+        Deletes a class instance of a given id"""
+        args = parse(line)
+        obdict = storage.all()
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(args[0], args[1]) not in obdict.keys():
+            print("** no instance found **")
+        else:
+            del obdict["{}.{}".format(args[0], args[1])]
+            storage.save()
 
->>>>>>> 1645a46337b5bc544a90deb1f8fbe5320274beb5
+    def do_update(self, line):
+        """Usage: update <class> <id> <attribute_name> <attribute_value> or
+        <class>.update(<id>, <attribute_name>, <attribute_value>) or
+        <class>.update(<id>, <dictionary>)
+        Update a class instance of a given id by adding or updating
+        a given attribute key/value pair or dictionary."""
+        args = parse(line)
+        obdict = storage.all()
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        if args[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+            return False
+        if len(args) == 1:
+            print("** instance id missing **")
+            return False
+        if "{}.{}".format(args[0], args[1]) not in obdict.keys():
+            print("** no instance found **")
+            return False
+        if len(args) == 2:
+            print("** attribute name missing **")
+            return False
+        if len(args) == 3:
+            try:
+                type(eval(args[2])) != dict
+            except NameError:
+                print("** value missing **")
+                return False
+        if len(args) == 4:
+            obj = obdict["{}.{}".format(args[0], args[1])]
+            if args[2] in obj.__class__.__dict__.keys():
+                valtype = type(obj.__class__.__dict__[args[2]])
+                obj.__dict__[args[2]] = valtype(args[3])
+            else:
+                obj.__dict__[args[2]] = args[3]
+        elif type(eval(args[2])) == dict:
+            obj = obdict["{}.{}".format(args[0], args[1])]
+            for k, v in eval(args[2]).items():
+                if (k in obj.__class__.__dict__.keys() and
+                        type(obj.__class__.__dict__[k]) in {str, int, float}):
+                    valtype = type(obj.__class__.__dict__[k])
+                    obj.__dict__[k] = valtype(v)
+                else:
+                    obj.__dict__[k] = v
+        storage.save()
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
